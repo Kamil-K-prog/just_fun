c5df0d53f8f6357835ee82a8a49bbaac1c821fd9
runner <you@example.com>
2022-06-26 01:30:03 +0500

    delete db

--- data/interface/interface_manager.py
+++ data/interface/interface_manager.py
@@ -2,7 +2,6 @@ from .all_interface import lobby
 from .all_interface import web
 
 
-
 def global_interface_init(app):
     app.register_blueprint(lobby.blueprint)
     app.register_blueprint(web.blueprint)

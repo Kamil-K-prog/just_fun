235ba9b559099d5b7ab577fc427c9f117fee4d63
Funky Monk <dimbv5210@gmail.com>
2022-07-21 17:05:20 +0500

    Теперь при запуске программы есть гарантия, что все обязательные для работы WEB-приложения записи в БД там уже есть. Добавлен легко расширяемый скрипт data/db/db_preparation_manager.py, отвечающий за это. Обновлён сам файл базы данных в директории database

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from config import AppConfig
-from data.db import db_sessionmaker
+from data.db import db_sessionmaker, db_preparation_manager
 from data.routes.all_routes import auth
 # from data.routes import routes_manager  # заглушено на время интеграции с бд
 from flask import render_template
@@ -18,6 +18,7 @@ if __name__ == '__main__':
         app.config['DB_FILENAME'])
     app.config['UPLOAD_FOLDER'] = "/upload/"
     db_sessionmaker.global_init(db_path)
+    db_preparation_manager.prepare_db()
     auth.register_login_manager(app)
     app.register_blueprint(auth.blueprint)
 
@@ -28,7 +29,7 @@ if __name__ == '__main__':
     @app.errorhandler(401)
     def page_not_found(e):
         return render_template('403.html'), 401
-    
+
     # заглушено на время интеграции с бд
     # routes_manager.global_routes_init(app)
     app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ data/db/db_preparation_manager.py
@@ -0,0 +1,46 @@
+from . import db_sessionmaker
+from .__all_models import Role, PassportStatus, FieldType, EvalMarkProfrisks, \
+    EvalMarkSout
+
+
+EVAL_MARK_CONTENTS = ('да', 'нет', 'частично')  # метки качества соблюдения
+PREPARATION_DATA = [  # Константа с обязательными записями БД (см. prepare_db)
+    # Роли пользователей
+    {'model': Role, 'name_field': 'name', 'contents': ('User', 'Admin')},
+    # Статусы паспортов организаций
+    {'model': PassportStatus, 'name_field': 'name',
+     'contents': ('Принят', 'На рассмотрении', 'Отклонен')},
+    # Типы полей в универсальной форме опросника
+    {'model': FieldType, 'name_field': 'name', 'contents': (
+        'Свободное текстовое поле', 'Текстовое поле с вариантами ответов',
+        'Дата', 'Число', 'Загрузка файла')},
+    # Метки качества и наличия оценки профессиональных рисков
+    {'model': EvalMarkProfrisks, 'name_field': 'title',
+        'contents': EVAL_MARK_CONTENTS},
+    # Метки качества и наличия специальной оценки условий труда (СОУТ)
+    {'model': EvalMarkSout, 'name_field': 'title',
+        'contents': EVAL_MARK_CONTENTS},
+]
+
+
+def prepare_db() -> None:
+    ("""Производит обязательные для корректной работы web-приложения записи """
+     """в базу данных, если на данный момент таковых в ней нет""")
+
+    with db_sessionmaker.create_session() as db_sess:
+        for essential_dict in PREPARATION_DATA:
+            model = essential_dict['model']  # класс ORM-модели
+            name_field = essential_dict['name_field']  # название поля названия
+
+            for model_obj_id, obj_name_value in enumerate(
+                    essential_dict['contents'], start=1):
+                # очередной обязательный для работы приложения объект модели
+                model_obj = db_sess.query(model).get(model_obj_id)
+
+                if model_obj is None:  # если ещё не создан, создаём
+                    model_obj = model(
+                        id=model_obj_id, **{name_field: obj_name_value})
+
+                    db_sess.add(model_obj)
+
+        db_sess.commit()
